ae0f06b0dbea50ee86b1c81db7aaa20cc7065067
AditMeh <aditya.mehrotra@mail.utoronto.ca>
2021-10-15 23:02:37 -0400

    renamed movable to traversable

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,14 +1,14 @@
 public class Tile {
-    private final boolean isMovable;
+    private final boolean isTraversable;
     private String tileImage;
 
     public Tile(boolean isMovable, String tileImage){
-        this.isMovable = isMovable;
+        this.isTraversable = isMovable;
         this.tileImage = tileImage;
     }
 
-    public boolean isMovable() {
-        return this.isMovable;
+    public boolean isTraversable() {
+        return this.isTraversable;
     }
 
     @Override
