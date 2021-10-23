175df49bcf2db85922112de60181628636087a3c
kakaomonk <park.sm.apple@gmail.com>
2021-10-23 18:45:03 -0400

    PlayerTest

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,3 +1,20 @@
-// TODO add tests for all the current player methods
+import org.junit.*;
+
 public class PlayerTest {
+    @Test(timeout = 50)
+    public void TestPlayer(){
+        Player player = new Player(25, "P");
+
+        assert(player.getPos() == (25));
+        player.setPos(27);
+        assert (player.getPos() == (27));
+
+        assert (player.getPoints() == (100));
+        player.setPoints(20);
+        assert (player.getPoints() == (120));
+
+        player.changePoints(10);
+        assert (player.getPoints() == 130);
+
+    }
 }
