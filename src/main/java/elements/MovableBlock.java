15a0efa2246d2e4cd724e51a66a36ea5a92f90f2
kakaomonk <park.sm.apple@gmail.com>
2021-11-08 21:46:56 -0500

    Added TODO

--- src/main/java/elements/MovableBlock.java
+++ src/main/java/elements/MovableBlock.java
@@ -4,7 +4,7 @@ public class MovableBlock extends Element implements Movable {
     public MovableBlock(String sprite, int[] pos) {
         super(sprite, pos);
     }
-
+    //TODO: MovableBlock should be "Pushed" when player moves to MovableBlock.
     @Override
     public void move() {
 
