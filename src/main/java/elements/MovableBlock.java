b61d59fd2d2e260ffac4625ca50d40595c4ad286
kakaomonk <park.sm.apple@gmail.com>
2021-11-09 12:44:46 -0500

    Updated TODO

--- src/main/java/elements/MovableBlock.java
+++ src/main/java/elements/MovableBlock.java
@@ -5,7 +5,7 @@ public class MovableBlock extends Element implements Movable {
         super(sprite, pos);
     }
     //TODO: MovableBlock should be "Pushed" when player moves to MovableBlock.
-    //If player push the block from left setPos(a+1, b), and note that
+    //If player push the block from left setPos(a, b+1), and note that the pos of the element.getPos() cannot exceed the (board.getSize() - 1 - num of elements in the ROW/COL).
     @Override
     public void move() {
 
