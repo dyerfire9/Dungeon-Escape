3b128a7604e6e80ecb51a4350451dfd1105ba3a8
szj17 <s.juni@mail.utoronto.ca>
2021-11-09 00:40:49 -0500

    Added basic Serialize and Deserialize classes

---
+++ src/main/java/Deserialize.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Deserialize {
+    public static Game Deserialize(String [] args){
+        Game game = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("/tmp/game.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            game = (Game) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+
+        } catch (ClassNotFoundException c) {
+            System.out.println("Game class not found");
+            c.printStackTrace();
+        }
+        return game;
+    }
+}
