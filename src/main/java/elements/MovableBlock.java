78f1cc328d3b5dfe8a4abd1d2db3a6de0546d1b4
kakaomonk <park.sm.apple@gmail.com>
2021-11-09 14:11:09 -0500

    Implemented MovableBlock.move()

--- src/main/java/elements/MovableBlock.java
+++ src/main/java/elements/MovableBlock.java
@@ -1,14 +1,16 @@
 package elements;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MovableBlock extends Element implements Movable {
     public MovableBlock(String sprite, int[] pos) {
         super(sprite, pos);
     }
-    //TODO: MovableBlock should be "Pushed" when player moves to MovableBlock.
-    //If player push the block from left setPos(a, b+1), and note that the pos of the element.getPos() cannot exceed the (board.getSize() - 1 - num of elements in the ROW/COL).
 
     @Override
     public void move() {
-
+        int[] movement = new int[]{this.getPos()[0] + ThreadLocalRandom.current().nextInt(-1, 1),
+                this.getPos()[1] + ThreadLocalRandom.current().nextInt(-1, 1)};
+        this.setPos(movement);
     }
 }
