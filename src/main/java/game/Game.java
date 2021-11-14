df69eb5601aaf21f1b264144ff8632a713b6b9e3
Gwen Gou <gwen.gou@mail.utoronto.ca>
2021-11-13 20:57:36 -0500

    Added JavaDocs for Game and Player classes.

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -14,6 +14,11 @@ public class Game {
     private Board board;
     private Player player;
 
+    /**
+     * A constructor for the Game class.
+     * Currently, each new Game is hard-coded to have a new Player at starting position (5, 5).
+     * @param size the size of the board, which is an arbitrary integer to be chosen by the human user
+     */
     public Game (int size) {
         this.board = new Board(size);
 
@@ -23,12 +28,16 @@ public class Game {
     }
 
 
+    /**
+     * Moves the Player to a new position.
+     * @param move the incremental movement the Player is to make from its current position.
+     */
     public void movePlayer(Point2D move) {
         Point2D currentPos = player.getPos();
         Point2D newPos = Point2D.add(currentPos, move);
-        boolean element = board.isTraversable(newPos);
+        boolean traversable = board.isTraversable(newPos);
 
-        if (element)  {
+        if (traversable)  {
             player.setPos(newPos);
         }
         else  {
@@ -37,10 +46,16 @@ public class Game {
     }
 
 
+    /**
+     * A wrapper method that calls on the underlying board to update itself.
+     */
     public void updateBoard() {
         this.board.updateBoard();
     }
 
+    /**
+     * Updates the Player's PlayerState using the board's feedback based on the Player's current location. Also decreases the Player's temporary invincibility-frame-count by 1.
+     */
     public void updatePlayerState() {
         PlayerState currPlayerState = this.player.playerState;
         Point2D position = this.player.getPos();
@@ -50,7 +65,10 @@ public class Game {
         this.player.decrementIframes();
     }
 
-    // Getter & Setter for game status.
+    /**
+     * Getters and Setters for game status.
+     */
+
     public boolean isRunning() {
         return this.isRunning;
     }
--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -7,6 +7,10 @@ public class Player {
     public PlayerState playerState;
     private final String sprite;
 
+    /**
+     * A constructor for the Player class, which sets its position on the board, its PlayerState with an initial 100 points, and a representation.
+     * @param pos the initial position of the Player
+     */
     public Player(Point2D pos){
         this.pos= pos;
         this.playerState = new PlayerState(100);
@@ -14,20 +18,26 @@ public class Player {
     }
 
 
+    /**
+     * Getters and Setters for Player attributes.
+     */
     public Point2D getPos(){
         return pos;
     }
     public void setPos(Point2D newPos) { this.pos = newPos; }
-
     public void setPlayerState(PlayerState playerState) {
         this.playerState = playerState;
     }
     public String getSprite() {
         return this.sprite;
     }
+    public PlayerState getPlayerState() {return this.playerState;}
+
+
+    /**
+     * A wrapper method that calls on the Player's PlayerState to decrease its invincibility-frame-count.
+     */
     public void decrementIframes() {
         this.playerState.decrementIframes();
     }
-    public PlayerState getPlayerState() {return this.playerState;}
-
 }
