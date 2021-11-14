9041d78f223f17cc5e9bd42cb2af4134d4da6463
Gwen Gou <gwen.gou@mail.utoronto.ca>
2021-11-13 20:16:27 -0500

    Add JavaDocs for Element class.

--- src/main/java/elements/Element.java
+++ src/main/java/elements/Element.java
@@ -9,19 +9,34 @@ public class Element {
     private Point2D pos;
 
 
+    /**
+     * A constructor fo the Element class. This is the base class for all elements on the board.
+     * @param sprite the element's representation; currently set to String, but will be mapped to an image by the GraphicsLoader.
+     * @param pos the element's position on the board, represented by a pair of integer coordinates.
+     */
     public Element(String sprite, Point2D pos){
         this.sprite = sprite;
         this.pos = pos;
     }
 
+    /**
+     * @return the mapping between the location of this element and its representation.
+     */
     public PointImagePair getPointImagePair() {
         return new PointImagePair(this.pos, this.sprite);
     }
 
+    /**
+     * @return the current position of the element
+     */
     public Point2D getPos() {
         return this.pos;
     }
 
+    /**
+     * Sets the element to a new position on the board.
+     * @param newPos new location of the element
+     */
     public void setPos(Point2D newPos) {
         this.pos = newPos;
     }
