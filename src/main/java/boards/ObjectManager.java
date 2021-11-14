c5e276cc1009af5adf0d4463215f0da370037e7c
Gwen Gou <gwen.gou@mail.utoronto.ca>
2021-11-13 19:39:11 -0500

    Add JavaDocs for ObjectManager class.

--- src/main/java/boards/ObjectManager.java
+++ src/main/java/boards/ObjectManager.java
@@ -13,6 +13,10 @@ public class ObjectManager {
     private ArrayList<Element> boardObjects;
     private int bound;
 
+    /**
+     * A constructor for ObjectManager class.
+     * @param bound the size of the board - 1, which is the size of the area where the elements can reside.
+     */
     public ObjectManager(int bound){
         this.boardObjects = new ArrayList<Element>();
         this.bound = bound;
@@ -20,11 +24,20 @@ public class ObjectManager {
         System.out.println(this.bound);
     }
 
+    /**
+     * Another constructor for the ObjectManager class, which creates an ObjectManager from a list of existing elements. Used to reload the game.
+     * @param boardObjects a list of existing elements
+     * @param bound the size of the board - 1, which is the size of the area where the elements can reside.
+     */
     public ObjectManager(ArrayList<Element> boardObjects, int bound){
         this.boardObjects = boardObjects;
         this.bound = bound;
     }
 
+    /**
+     * Add new elements to the board, collected and managed by the objectManager.
+     * @param object new element to be added
+     */
     public void addObject(Element object){
         boardObjects.add(object);
     }
@@ -32,29 +45,61 @@ public class ObjectManager {
     // Methods to enable calls from gameMaker
     // TODO: add more or configure with gameMaker
 
+    /**
+     * A method to place, specifically, an AlligatorDen that shoots alligators only to the right on the board. This method will link with gameMaker's "elements menu," so that the user can choose a specific type of element to plant at a specific location on the board.
+     * @param pos where the new AlligatorDen is placed on the board.
+     */
     public void addRightAlligatorDen(Point2D pos) {
         this.addObject(new AlligatorDen("alligatorDen", pos, new Point2D(1,0) ,120, bound));
     }
+
+
+    /**
+     * A method to place left-shooting AlligatorDens.
+     * @param pos where the new AlligatorDen is placed on the board
+     */
     public void addLeftAlligatorDen(Point2D pos) {
         this.addObject(new AlligatorDen("alligatorDen", pos, new Point2D(-1,0) ,120, bound));
     }
+
+    /**
+     * A method to place upward-shooting AlligatorDens.
+     * @param pos where the new AlligatorDen is placed on the board
+     */
     public void addUpAlligatorDen(Point2D pos) {
         this.addObject(new AlligatorDen("alligatorDen", pos, new Point2D(0,-1) ,120, bound));
     }
+
+    /**
+     * A method to place downward-shooting AlligatorDens.
+     * @param pos where the new AlligatorDen is placed on the board
+     */
     public void addDownAlligatorDen(Point2D pos) {
         this.addObject(new AlligatorDen("alligatorDen", pos, new Point2D(0, 1) ,120, bound));
     }
 
+    /**
+     * A method to place a Goal element on the board. When Player reaches the Goal, the Player wins the game.
+     * @param pos location where the Goal element is placed on the board
+     */
     public void addGoal(Point2D pos) {
         this.addObject(new Goal("Goal", pos));
     }
 
 
-
+    /**
+     * A method to remove elements from the board, which is managed by the board's objectManager.
+     * @param object the element to be deleted
+     */
     public void removeObject(Element object){
         boardObjects.remove(object);
     }
 
+
+    /**
+     * A getter method to collect and access a mapping between the location and the string representation of all objects currently on the board.
+     * @return the collection of every object's location and corresponding string representation.
+     */
     public ArrayList<PointImagePair> getPointImagePairs(){
         ArrayList<PointImagePair> collector = new ArrayList<>();
         for (Element boardObject : this.boardObjects) {
@@ -63,6 +108,9 @@ public class ObjectManager {
         return collector;
     }
 
+    /**
+     * Updates the content of the ObjectManager, removing elements that have gone off the screen and adding elements that have been generated.
+     */
     public void updateObjects(){
         HashSet<Element> objectsToRemove = new HashSet<>();
         HashSet<Element> objectsToAdd = new HashSet<>();
