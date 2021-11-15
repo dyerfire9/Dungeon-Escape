99560e70cfc4ce4ecb58e4aa5879f90b6055db35
Hyungsoo Park <park.sm.apple@gmail.com>
2021-11-14 19:41:25 -0500

    Deleted old version of BoardTest lines

--- src/test/java/boards/BoardTest.java
+++ src/test/java/boards/BoardTest.java
@@ -13,40 +13,4 @@ public class BoardTest {
         assert board.isTraversable(a);
         assert !board.isTraversable(b);
     }
-
-    //@Test(timeout = 50)
-    //public void TestBoardToString_1() {
-    //    Board board = new Board(10);
-    //    assert(board.toString().equals("Board{size=10, board=[x, x, x, x, x, x, x, x, x, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, x, x, x, x, x, x, x, x, x, null]}"));
-    //}
-
-    //@Test(timeout =50)
-    //public void TestBoardToString_2() {
-    //    Board board = new Board(15);
-    //    assert(board.toString().equals("Board{size=15, board=[x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, null, null, null, null, null, null, null, null, null, null, null, null, null, x, null, " +
-    //            "x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, null]}"));
-
-    //}
 }
