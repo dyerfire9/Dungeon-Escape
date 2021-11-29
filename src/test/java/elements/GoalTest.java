41df858b9f3923b7a4a45d9a19af81958b4e509e
AditMeh <aditya.mehrotra@mail.utoronto.ca>
2021-11-29 04:07:14 -0500

    uncommented goalTest, this doesn't work

--- src/test/java/elements/GoalTest.java
+++ src/test/java/elements/GoalTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 import game.PlayerState;
 import utils.Point2D;
 
-//public class GoalTest {
-//    @Test(timeout = 500)
-//    public void TestReachGoal() {
-//        Goal goal = new Goal("goal", new Point2D(2, 2));
-//        PlayerState playerState = new PlayerState(100);
-//        goal.changePlayerState(playerState);
-//        assert (playerState.getWinningState());
-//    }
-//}
+public class GoalTest {
+    @Test(timeout = 500)
+    public void TestReachGoal() {
+        Goal goal = new Goal("goal", new Point2D(2, 2));
+        PlayerState playerState = new PlayerState(100, Point2D);
+        goal.changePlayerState(playerState);
+        assert (playerState.getWinningState());
+    }
+}
