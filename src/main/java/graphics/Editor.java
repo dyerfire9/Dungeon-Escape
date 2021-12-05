b9d2b36d82cdc5365804af52d2e54bce64b42dbd
Alfglass <rainyday.tea.party@gmail.com>
2021-12-04 22:00:50 -0500

    Fleshed out Editor, changed PaletteButton

--- src/main/java/graphics/Editor.java
+++ src/main/java/graphics/Editor.java
@@ -1,32 +1,63 @@
 package graphics;
 
 import graphics.enums.ToolMode;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import utils.EnumsForSprites;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
+/**
+ * Presenter class for the game's editor pane. This pane contains tool buttons for placing and deleting
+ * and a "palette" of buttons representing elements that the user can select to place.
+ */
 public class Editor {
+
     private static final File TEMPLATE = new File("src/main/assets/main.fxml");
 
     private EnumsForSprites selectedElement;
     private ToolMode toolMode;
-    private ScrollPane root;
-    private PaletteButton template;
+    private Pane root;
+    private VBox paletteContainer;
+    private Button addTool;
+    private Button deleteTool;
+    private ArrayList<PaletteButton> buttons;
 
+    /**
+     * Creates a new instance with default property values.
+     * @throws IOException If there is an error in loading the FXML template.
+     */
     public Editor() throws IOException {
         URL url = TEMPLATE.toURI().toURL();
         Scene sc = new Scene(FXMLLoader.load(url));
-        root = (ScrollPane) sc.lookup("#editor");
+        root = (Pane) sc.lookup("#editor");
+        addTool = (Button) sc.lookup("#addToolButton");
+        deleteTool = (Button) sc.lookup("#deleteToolButton");
+        paletteContainer = (VBox) sc.lookup("palette");
+        buttons = new ArrayList<>();
+    }
 
-        Button button = (Button) sc.lookup("#templateButton");
-        ImageView imgView = (ImageView) sc.lookup("#templateButtonImg");
+    /**
+     * Adds a new button to the editor's palette.
+     * @param image The image displayed on the new button.
+     * @param handler The listener that is fired when the button is clicked.
+     * @throws IOException If there was an error in creating the new button, likely due to
+     *      loading the FXML template.
+     */
+    public void addPaletteButton(Image image, EventHandler<MouseEvent> handler) throws IOException {
+        PaletteButton pb = new PaletteButton(image);
+        pb.setOnClicked(handler);
+        buttons.add(pb);
+        paletteContainer.getChildren().add(pb.getButton());
     }
 
     /**
@@ -41,5 +72,25 @@ public class Editor {
      */
     public void show() {
         root.setVisible(true);
+    }
+
+    /**
+     * Gets the selected palette element.
+     * @return
+     */
+    public EnumsForSprites getSelectedElement() {
+        return selectedElement;
+    }
+
+    public void setSelectedElement(EnumsForSprites selectedElement) {
+        this.selectedElement = selectedElement;
+    }
+
+    public ToolMode getToolMode() {
+        return toolMode;
+    }
+
+    public void setToolMode(ToolMode toolMode) {
+        this.toolMode = toolMode;
     }
 }
--- src/main/java/graphics/PaletteButton.java
+++ src/main/java/graphics/PaletteButton.java
@@ -1,10 +1,12 @@
 package graphics;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,11 +18,24 @@ public class PaletteButton {
     private ImageView imageView;
     private Button button;
 
+    /**
+     * Creates a new button from an FXML template, with a specified button.
+     * @param img The specified image.
+     * @throws IOException Thrown if there is an error in loading the FXML file.
+     */
     public PaletteButton(Image img) throws IOException {
         URL url = TEMPLATE.toURI().toURL();
         Scene sc = new Scene(FXMLLoader.load(url));
         button = (Button) sc.lookup("#button");
-
+        imageView = (ImageView) sc.lookup("#img");
+        imageView.setImage(img);
     }
 
+    public void setOnClicked(EventHandler<MouseEvent> handler) {
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, handler);
+    }
+
+    public Button getButton() {
+        return button;
+    }
 }
