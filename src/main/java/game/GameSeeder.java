443fe9db8f1f1ea19b2b51b5e37600d78a3b9ac7
Gwen Gou <gwen.gou@mail.utoronto.ca>
2021-12-08 00:39:43 -0500

    Moved seeding methods in RenderPane to GameSeeder. Added JavaDocs to multiple classes.

--- src/main/java/game/GameSeeder.java
+++ src/main/java/game/GameSeeder.java
@@ -1,53 +1,124 @@
 package game;
 
 import elements.types.PushableElement;
+import utils.EnumsForSprites;
 import utils.Point2D;
 
 public class GameSeeder {
     private Game game;
 
+    /**
+     * A contructor for this class.
+     * @param g the Game object to which this GameSeeder applies.
+     */
     public GameSeeder(Game g) {
         this.game = g;
     }
 
+    /**
+     * A wrapper method for this GameSeeder to add a Goal object on its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addGoal (Point2D pos) {
         this.game.getObjectManager().addGoal(pos);
     }
 
+    /**
+     * A wrapper method for this GameSeeder to add a downward-shooting AlligatorDen object to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addDownAlligatorDen(Point2D pos) {
         this.game.getObjectManager().addDownAlligatorDen(pos);
     }
 
+
+    /**
+     * A wrapper method for this GameSeeder to add a upward-shooting AlligatorDen object to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addUpAlligatorDen(Point2D pos) {
         this.game.getObjectManager().addUpAlligatorDen(pos);
     }
 
+
+    /**
+     * A wrapper method for this GameSeeder to add a left-shooting AlligatorDen object to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addLeftAlligatorDen(Point2D pos) {
         this.game.getObjectManager().addLeftAlligatorDen(pos);
     }
 
+
+    /**
+     * A wrapper method for this GameSeeder to add a right-shooting AlligatorDen object to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addRightAlligatorDen(Point2D pos) {
         this.game.getObjectManager().addRightAlligatorDen(pos);
     }
 
+
+    /**
+     * A wrapper method for this GameSeeder to add a ChasingElement to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addChasingElement(Point2D pos, int max_tick) {
         this.game.getObjectManager().addChasingElement(pos, max_tick);
     }
+
+    /**
+     * A wrapper method for this GameSeeder to add a Portal to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
 
     public void addPortal(Point2D pos) {
         this.game.getObjectManager().addPortal(pos);
     }
 
+
+    /**
+     * A wrapper method for this GameSeeder to add a Rock object to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addRock(Point2D pos) {
         this.game.getObjectManager().addRock(pos);
     }
 
+
+    /**
+     * A wrapper method for this GameSeeder to add a Pushable object to its Game.
+     * @param pos the position on the Game's board where the element is to be planted
+     */
     public void addPushable(Point2D pos)  {
         if (PushableElement.instanceCounter <= 1) {
             this.game.getObjectManager().addPushable(pos);
         }
         else {
             System.out.println("Can't plant more PushableElements.");
+        }
+    }
+
+    public void add(EnumsForSprites element, Point2D mousePos) {
+        if (element == EnumsForSprites.GOAL) {
+            this.addGoal(mousePos);
+        } else if (element == EnumsForSprites.ALLIGATOR_DEN_UP) {
+            this.addUpAlligatorDen(mousePos);
+        } else if (element == EnumsForSprites.ALLIGATOR_DEN_DOWN) {
+            this.addDownAlligatorDen(mousePos);
+        } else if (element == EnumsForSprites.ALLIGATOR_DEN_LEFT) {
+            this.addLeftAlligatorDen(mousePos);
+        } else if (element == EnumsForSprites.ALLIGATOR_DEN_RIGHT) {
+            this.addRightAlligatorDen(mousePos);
+        } else if (element == EnumsForSprites.PORTAL) {
+            this.addPortal(mousePos);
+        } else if (element == EnumsForSprites.CHASER) {
+            this.addChasingElement(mousePos, 15);
+        } else if (element == EnumsForSprites.ROCK) {
+            this.addRock(mousePos);
+        }
+        else {
+            System.out.printf("No implementation for placing element '%s'.%n", element);
         }
     }
 
