9e76d9cb946d19314aa13c4f1e57781e1b27aa88
szj17 <s.juni@mail.utoronto.ca>
2021-12-07 23:14:29 -0500

    Updated AlligatorDenTest

--- src/test/java/elements/types/AlligatorDenTest.java
+++ src/test/java/elements/types/AlligatorDenTest.java
@@ -9,7 +9,8 @@ import utils.EnumsForSprites;
 public class AlligatorDenTest {
     @Test(timeout = 500)
     public void TestAlligatorDenGenerate() {
-        AlligatorDen alligatorDen = new AlligatorDen(EnumsForSprites.ALLIGATOR_DEN_RIGHT, new Point2D(3,3),
+        Point2D point2D = new Point2D(3,3);
+        AlligatorDen alligatorDen = new AlligatorDen(EnumsForSprites.ALLIGATOR_DEN_RIGHT, point2D,
                 new Point2D(1, 0), 60, 10, true);
 
         for (int i = 0; i <= 59; i++) {
@@ -17,6 +18,8 @@ public class AlligatorDenTest {
         }
         MovableElement element = alligatorDen.placeElement();
 
+
         assert (!(element == null));
+        assert alligatorDen.generateElement(point2D,4).getPos() == point2D;
     }
 }
