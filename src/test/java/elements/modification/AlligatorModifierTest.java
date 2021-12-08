bf8ef95de78c5d0ff5a2c9226be3db5bff4f9b47
szj17 <s.juni@mail.utoronto.ca>
2021-12-07 22:51:56 -0500

    Reflecting repackaged elements, changes in code - without actually writing new tests, just editing what was there to let it run

---
+++ src/test/java/elements/modification/AlligatorModifierTest.java
@@ -0,0 +1,38 @@
+package elements.modification;
+
+import elements.types.Alligator;
+import org.junit.Test;
+import game.PlayerState;
+import utils.Point2D;
+import utils.EnumsForSprites;
+
+public class AlligatorModifierTest {
+    @Test(timeout = 500)
+    public void TestAlligatorModifier(){
+        Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
+                20, 60, new Point2D(1, 0), false);
+
+        Point2D point2D = new Point2D(2,2);
+        PlayerState playerState = new PlayerState(100, point2D);
+
+        AlligatorModifier modifiedPlayerstate =  alligator.Modify(playerState);
+        assert (modifiedPlayerstate.Modifier(playerState).getPoints() == 100);
+    }
+
+    @Test(timeout = 500)
+    public void TestAlligatorInteractionVulnerable() {
+        Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
+                20, 60, new Point2D(1, 0), false);
+
+        Point2D point2D = new Point2D(6,5);
+        PlayerState playerState = new PlayerState(100, point2D);
+
+        for (int i = 0; i <= 60; i ++) {
+            playerState.decrementIframes();
+        }
+
+        AlligatorModifier modifiedPlayerstate =  alligator.Modify(playerState);
+        assert (modifiedPlayerstate.Modifier(playerState).getPoints() == 99);
+    }
+
+}
