4ef70fe35ce8e068121f73f5069e299714c2bcb1
szj17 <s.juni@mail.utoronto.ca>
2021-12-07 23:04:29 -0500

    Updated AlligatorTest

--- src/test/java/elements/AlligatorTest.java
+++ src/test/java/elements/AlligatorTest.java
@@ -8,47 +8,12 @@ import utils.EnumsForSprites;
 
 public class AlligatorTest {
         @Test(timeout = 500)
-        public void TestAlligatorMovement() {
-            Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
-                    20, 60, new Point2D(1, 0), false);
-
-            for (int i = 0; i <= 60; i++) {
-                alligator.processTick();
-            }
-            assert (Point2D.equals(alligator.getPos(), new Point2D(7, 5)));
+        public void TestAlligatorConstructor(){
+        Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
+                20, 60, new Point2D(1, 0), false);
+        Point2D point2D = new Point2D(2,2);
+        PlayerState playerState = new PlayerState(100, point2D);
+        assert alligator.Modify(playerState).Modifier(playerState).getPoints() == 100;
         }
-        @Test(timeout = 500)
-        public void TestAlligatorNoMove() {
-            Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
-                    20, 60, new Point2D(1, 0), false);
 
-            for (int i = 0; i <= 59; i++) {
-                alligator.processTick();
-            }
-            assert (!Point2D.equals(alligator.getPos(), new Point2D(7, 5)));
-        }
-        @Test(timeout = 500)
-        public void TestAlligatorInteractionInvincible() {
-            Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
-                    20, 60, new Point2D(1, 0), false);
-
-            PlayerState playerState = new PlayerState(100);
-
-            PlayerState modifiedPlayerstate =  alligator.changePlayerState(playerState);
-            assert (modifiedPlayerstate.getPoints() == 100);
-        }
-        @Test(timeout = 500)
-        public void TestAlligatorInteractionVulnerable() {
-            Alligator alligator = new Alligator(EnumsForSprites.ALLIGATOR, new Point2D(6, 5),
-                    20, 60, new Point2D(1, 0), false);
-
-            PlayerState playerState = new PlayerState(100);
-
-            for (int i = 0; i <= 60; i ++) {
-                playerState.decrementIframes();
-            }
-
-            PlayerState modifiedPlayerstate =  alligator.changePlayerState(playerState);
-            assert (modifiedPlayerstate.getPoints() == 99);
-        }
-    }
+}
